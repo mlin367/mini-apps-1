e5af73e68c6514301540a4535f0f5b6ec7a4bd1d
Matthew Lin <mlin367@yahoo.com>
2019-02-11 17:50:47 -0800

    able to send info to server

--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(express.static(path.join(__dirname, './client')));
+
+app.post('/', (req, res) => {
+  console.log(JSON.parse(req.body.reqData));
+  let obj = JSON.parse(req.body.reqData);
+
+  // res.status(201).send('<form><input type="text"></form>');
+})
 
 app.listen(8008, () => {
   console.log('app is listening on port 8008');
